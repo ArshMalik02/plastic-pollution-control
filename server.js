cd12afc2977f15b622829d55b4a9aa820de7fe76
arsh <arshmalik02@gmail.com>
2019-06-15 15:06:20 +0530

    another commit

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express=require('express')
 const app=express()
 const path=require('path')
-const port=5000 
+const port=process.env.port
+const fs=require('fs')
 
-let users=[]
+let users
+let data2
+fs.readFile('./users.txt',(err,data)=>{
+     if(err) console.log(err)
+     else {
+          data2=data
+          try{
+               users=JSON.parse(data2)
+          }catch(e){
+               users=[]
+          }
+          
+     }
+})
 
+
+
+
+app.set('view engine', 'hbs')
 
 
 app.use(express.json())
@@ -18,6 +36,9 @@ app.post('/SignUp', function(req,res,next){
         
 users.push({...req.body,donations:[]})
       res.sendFile(path.join(__dirname,"/Static/html/welcome.html"))
+      fs.writeFile('users.txt', JSON.stringify(users),(err)=>{
+           console.log(err)
+      })
 }
 )
 
@@ -26,6 +47,9 @@ for(let i in users){
      if(users[i].email==req.body.email){
           let user=users[i]
           user.donations.push(req.body)
+          fs.writeFile('users.txt', JSON.stringify(users),(err)=>{
+               console.log(err)
+          })
 
      }
 
@@ -40,34 +64,23 @@ app.post('/login', function(req,res,next){
        if(i.email==req.body.email && i.password==req.body.password){
         name=i.name
         email=i.email
-        
-        donate=i.donations[0]
+        if(i.donations[0]){
+             donate=i.donations[0].donate
+        }else{
+             donate=0
+        }
        res.render('dashboard.hbs',
        {name:name,
        email:email,
-       donate: donate.donate})
+       donate: donate})
        }
        else{
           res.send("<h1>Not Authorized</h1>")
        }
 
      }
-     
-
-     
-
+ 
 })
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, function(){
     console.log('Port started')
